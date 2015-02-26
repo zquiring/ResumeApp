1c0164e63230ef26d6281c4e7c557aab3a72b688
user <user@users-MacBook-Pro.local>
2015-02-25 17:18:16 -0800

    Initial Commit

---
+++ ResumeApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ResumeApp
+//
+//  Created by user on 2/25/15.
+//  Copyright (c) 2015 ZachQuiring. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
